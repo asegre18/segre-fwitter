bb2e0f0513a41f9383b127e0a5e088210c1884d6
emmanueljucaban <emmanueljucaban@gmail.com>
2020-10-03 10:58:51 -0700

    F - Created insertToDb query, created insertUserApi in userController.js created an ORM for insertUser into db

--- model/userOrm.js
+++ model/userOrm.js
@@ -1,4 +1,4 @@
-const { findAllUsers } = require('./userQueries');
+const { findAllUsers, insertUserQuery } = require('./userQueries');
 const connection = require('../config/connection');
 
 const fetchUsers = async () => {
@@ -10,6 +10,16 @@ const fetchUsers = async () => {
   }
 };
 
+const insertUserToDb = async (username) => {
+  try {
+    const [result] = await connection.query(insertUserQuery, username);
+    return result;
+  } catch (e) {
+    throw new Error(e);
+  }
+};
+
 module.exports = {
   fetchUsers,
+  insertUserToDb,
 };
